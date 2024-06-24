a999e639a7742966747efc637ec95424701006b0
varad choudhari <121612372+V9494@users.noreply.github.com>
2024-06-24 20:06:23 +0530

    Added the touchmove and touchend for the mobile interface

--- script.js
+++ script.js
@@ -108,6 +108,8 @@ fileInputDesigns.addEventListener("change", () => {
             // Add dragstart and dragend listeners to the new image
             img.addEventListener("dragstart", drag);
             img.addEventListener("dragend", dragEnd);
+            img.addEventListener('touchmove',drag);
+            img.addEventListener('touchend',dragEnd)
 
             // Add click event listener to the remove button
             removeItem.addEventListener("click", (event) => {
